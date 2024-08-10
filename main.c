e73b8262b17251322fc85acae5bef7733faa80f8
gkloh <gabriel.loh@student.unimelb.edu.au>
2024-08-11 01:26:25 +1000

    Comments and testing commits

--- main.c
+++ main.c
@@ -12,4 +12,19 @@ Special Cases:
 Official Code State, Official Code Local Government Area: comma-separated lists of integers (treated as strings for this assignment).
 comma-containing strings: enclosed in double quotes ("") within the CSV file,
 the quotes are removed when stored in your program according to standard CSV rules.
+Suburbs might be in multiple local governments areas and multiple states. This is why the fields Official Code State
+and Official Code Local Government Area are not classified as integers. Each of these fields is actually a comma-separated list of integers. For the purposes of this assignment, it is fully sufficient to consider this as a string.
        */
+
+//*******************DEFINITIONS*******************************
+
+#define
+
+//**********************MAIN CODE******************************
+
+/*
+    Stage 1 will implement the lookup of data by a given key (Official Name Suburb) and
+
+    Stage 2 will implement the deletion of data by a given key (Official Name Suburb),
+    with the remaining data being output after all deletions are finished.\
+*/
