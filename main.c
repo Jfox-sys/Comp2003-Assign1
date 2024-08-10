8e54ad638f72e2f578f3290c4194340ba054d2ae
gkloh <gabriel.loh@student.unimelb.edu.au>
2024-08-11 01:02:53 +1000

    Comments and testing commits

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
-//test
+
+/* COMP2003 Dictionary Assignment 1
+    Gabriel Loh - 1462110, and Z_______ - _______
+    11/08/24 */
+
+/*     DATA TYPES:
+COMP20003 Code, Official Code Suburb, Year: integers
+Longitude, Latitude: doubles
+all other fields: strings (may contain spaces and commas)
+
+Special Cases:
+Official Code State, Official Code Local Government Area: comma-separated lists of integers (treated as strings for this assignment).
+comma-containing strings: enclosed in double quotes ("") within the CSV file,
+the quotes are removed when stored in your program according to standard CSV rules.
+       */
